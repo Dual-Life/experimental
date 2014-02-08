69f852f12e0c51513b7b8d8ebcf7c44ee2d10bea
Ricardo Signes <rjbs@cpan.org>
2014-02-08 12:52:41 -0500

    add "regex_sets" support

--- lib/experimental.pm
+++ lib/experimental.pm
@@ -13,6 +13,7 @@ my %min_version = (
 	array_base    => 5,
 	autoderef     => 5.014000,
 	lexical_topic => 5.010000,
+	regex_sets    => 5.018000,
 	smartmatch    => 5.010001,
 	signatures    => 5.019009, # change to 5.20.0 someday? -- rjbs, 2014-02-08
 );
