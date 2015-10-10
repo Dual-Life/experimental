b47239dc4a8e0cc847e57c0e6da1832493bf3df9
Leon Timmermans <fawaka@gmail.com>
2015-10-10 12:44:18 +0200

    Fix deprecation check

--- lib/experimental.pm
+++ lib/experimental.pm
@@ -43,6 +43,7 @@ my %max_version = (
 );
 
 $_ = version->new($_) for values %min_version;
+$_ = version->new($_) for values %max_version;
 
 my %additional = (
 	postderef  => ['postderef_qq'],
@@ -60,9 +61,6 @@ sub _enable {
 		feature->import($pragma);
 		_enable(@{ $additional{$pragma} }) if $additional{$pragma};
 	}
-	elsif ($] >= $max_version{$pragma}) {
-		croak "Experimental feature $pragma has been removed from perl in version $max_version{$pragma}";
-	}
 	elsif (not exists $min_version{$pragma}) {
 		croak "Can't enable unknown feature $pragma";
 	}
@@ -74,6 +72,9 @@ sub _enable {
 			);
 		}
 		croak "Need perl $stable or later for feature $pragma";
+	}
+	elsif ($] >= ($max_version{$pragma} || 7)) {
+		croak "Experimental feature $pragma has been removed from perl in version $max_version{$pragma}";
 	}
 }
 
